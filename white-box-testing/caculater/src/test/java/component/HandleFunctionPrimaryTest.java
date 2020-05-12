ef84c7e64c7939f0ab0defea87f7a09de3955053
chuyuxuan123 <chuyuxuan@126.com>
2020-05-12 16:03:57 +0800

    add test file

---
+++ white-box-testing/caculater/src/test/java/component/HandleFunctionPrimaryTest.java
@@ -0,0 +1,63 @@
+package component;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HandleFunctionPrimaryTest {
+
+    HandlerFunction handlerFunction = new HandlerFunction();
+
+    @Test
+    public void testPrimary001() {
+        String expression = "3;";
+        Token_Stream ts = new Token_Stream(expression);
+        Error error = new Error();
+        double var = handlerFunction.primary(0, ts, error);
+        Assert.assertEquals(var, 3, 0.0001);
+    }
+
+    @Test
+    public void testPrimary002() {
+        String expression = "+4;";
+        Token_Stream ts = new Token_Stream(expression);
+        Error error = new Error();
+        double var = handlerFunction.primary(3, ts, error);
+        Assert.assertEquals(var, 7, 0.0001);
+    }
+
+    @Test
+    public void testPrimary003() {
+        String expression = "-3;";
+        Token_Stream ts = new Token_Stream(expression);
+        Error error = new Error();
+        double var = handlerFunction.primary(4, ts, error);
+        Assert.assertEquals(1, var, 0.0001);
+    }
+
+    @Test
+    public void testPrimary004() {
+        String expression = "(4-2;";
+        Token_Stream ts = new Token_Stream(expression);
+        Error error = new Error();
+        double var = handlerFunction.primary(0, ts, error);
+        Assert.assertEquals(error.getError(), 5);
+    }
+
+    @Test
+    public void testPrimary005() {
+        String expression = "(4-2);";
+        Token_Stream ts = new Token_Stream(expression);
+        Error error = new Error();
+        double var = handlerFunction.primary(0, ts, error);
+        Assert.assertEquals(var, 2, 0.0001);
+    }
+
+    @Test
+    public void testPrimary006() {
+        String expression = ");";
+        Token_Stream ts = new Token_Stream(expression);
+        Error error = new Error();
+        double var = handlerFunction.primary(0, ts, error);
+        Assert.assertEquals(error.getError(), 6);
+    }
+}
---
+++ white-box-testing/caculater/src/test/java/component/HandlerFunctionSingleTest.java
@@ -0,0 +1,39 @@
+package component;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HandlerFunctionSingleTest {
+
+    HandlerFunction handlerFunction = new HandlerFunction();
+
+    // test case 3!
+    @Test
+    public void testSingle001() {
+        String expression = "3!;";
+        Token_Stream ts = new Token_Stream(expression);
+        Error error = new Error();
+        double var = handlerFunction.single(ts, error);
+        Assert.assertEquals(var, 6, 0.0001);
+    }
+
+    // test case 3.5!
+    @Test
+    public void testSingle002() {
+        String expression = "3.5!;";
+        Token_Stream ts = new Token_Stream(expression);
+        Error error = new Error();
+        double var = handlerFunction.single(ts, error);
+        Assert.assertEquals(error.getError(), 4);
+    }
+
+    // test case 3
+    @Test
+    public void testSingle003() {
+        String expression = "3;";
+        Token_Stream ts = new Token_Stream(expression);
+        Error error = new Error();
+        double var = handlerFunction.single(ts, error);
+        Assert.assertEquals(var, 3, 0.0001);
+    }
+}
